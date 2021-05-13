14af58f5a9cd69b77888cfb89b589748947d5c96
Poyu781 <v3708599@gmail.com>
2021-05-13 16:24:13 +0800

    finish imdb fetch python

--- DataFetch/imdb_fetch.py
+++ DataFetch/imdb_fetch.py
@@ -1,11 +1,21 @@
 import requests
 import urllib.request
-import ssl
+import ssl,os
 import gzip
+import dotenv
+import datetime
 from Modules.MySQL_module import SQL
+today_date = datetime.datetime.today().date()
+print(today_date)
+
+dotenv.load_dotenv()
+rds_host = os.getenv("rds_host")
+rds_user = os.getenv("rds_user")
+rds_password = os.getenv("rds_password")
+movie_database = SQL(user=rds_user,password=rds_password,host=rds_host,database="movie")
 
 
-# from Modules.MySQL_module import SQL
+# r = 1/0
 class IMDb_fetch():
     ssl._create_default_https_context = ssl._create_unverified_context
     INSTALL_PATH = "DataSection/"
@@ -19,56 +29,50 @@ class IMDb_fetch():
     
     def decompress_file(self):
         file_path = self.INSTALL_PATH + self.file_name
-        out_list = []
         with gzip.open(file_path) as f:
             file_content = f.read().decode("utf-8")
             data = file_content.split("\n")
-            for line in data[0:20] :
-                out_list.append(line.split('\t'))
             f.close()
-        return out_list
+        data.pop(0)
+        data.pop(-1)
+        return data
 
 movie_rate = IMDb_fetch("https://datasets.imdbws.com/title.ratings.tsv.gz","movie_rate.tsv.gz")
-movie_rate.install_data()
-x = movie_rate.decompress_file()
-print(x[:12])
+# movie_rate.install_data()
+rating_list = movie_rate.decompress_file()
+print(len(rating_list))
 
-# movie_detail = IMDb_fetch("https://datasets.imdbws.com/title.basics.tsv.gz","movie_detail.tsv.gz")
+def insert_rating(data):
+    insert_rating_list = []
+    for i in data :
+        movie_rate_info = i.split("\t")
+        movie_rate_info[1] = float(movie_rate_info[1])
+        movie_rate_info[2] = int(movie_rate_info[2])
+        movie_rate_info.append(today_date)
+        # print(movie_rate_info)
+        insert_rating_list.append(movie_rate_info)
+    movie_database.bulk_execute("insert IGNORE into `IMDb_rating` (`IMDb_id`,`rating`,`rating_count`,`update_date`) values(%s, %s, %s, %s)",insert_rating_list)
+    print("successful insert rating")
+
+
+movie_detail = IMDb_fetch("https://datasets.imdbws.com/title.basics.tsv.gz","movie_detail.tsv.gz")
 # movie_detail.install_data()
-# x = movie_detail.decompress_file()
-# print(x[:12])
-
-
-
-# url = "https://datasets.imdbws.com/title.ratings.tsv.gz"
-
-
-
-# urllib.request.urlretrieve(url, '/Users/poyuchiu/Desktop/MovieOn/DataSection/rate.tsv.gz')
-# with gzip.open('/Users/poyuchiu/Desktop/MovieOn/DataSection/rate.tsv.gz') as f:
-#     file_content = f.read().decode("utf-8")
-#     data = file_content.split("\n")
-#     print(data[0:15])
-#     f.close()
-    # print(len(f))
-    # f[-1] = f[-1] +'\n'
-    # r = 0
-    # for i in f:
-    #     split_data = i.split("\t")
-    #     try:
-    #         if split_data[1] == "movie" and  split_data[5] != '\\N' and int(split_data[5]) == 2021:
-    #             split_data[-1] = split_data[-1][:-2]
-    #             r += 1
-    #             print(split_data)
-    #     except:
-    #         print(split_data)
-    # print(r)
-
-# with open("/Users/poyuchiu/Desktop/MovieOn/DataSection/rate.tsv","w") as file:
-#     file.write(file_content)
-#     file.close()
-
-# with open("/Users/poyuchiu/Desktop/MovieOn/DataSection/rate.tsv","r") as file:
-#     print(file.readlines()[3:15])
-#     file.close()
-
+# detail_list = movie_detail.decompress_file()
+# print(len(detail_list))
+def insert_detail(data):
+    insert_detail_list = []
+    for i in data :
+        movie_detail_info = i.split("\t")
+        try:
+            if movie_detail_info[1] == "movie" and  movie_detail_info[5] != '\\N' and movie_detail_info[7] != '\\N' and int(movie_detail_info[5]) > 1980:
+                movie_detail_info.pop(1)
+                movie_detail_info.pop(5)
+                movie_detail_info[4] = int(movie_detail_info[4])
+                movie_detail_info[5] = int(movie_detail_info[5])
+                insert_detail_list.append(movie_detail_info)
+        except:
+            print(movie_detail_info)
+    movie_database.bulk_execute("INSERT INTO `movie_detail`(`IMDb_id`, `primary_title`, `original_title`, `is_adult`, `start_year`, `runtime_minutes`, `category`) VALUES (%s,%s,%s,%s,%s,%s,%s)",insert_detail_list)
+insert_rating(rating_list)
+# insert_detail(detail_list)
+print("si")
