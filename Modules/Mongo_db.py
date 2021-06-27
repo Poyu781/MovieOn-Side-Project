76561782daecd09ae024e3c36413ffdfcae3815d
Poyu781 <v3708599@gmail.com>
2021-06-27 21:02:24 +0800

    modify DataFetch file and Modules file

--- Modules/Mongo_db.py
+++ Modules/Mongo_db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 # from config import mongo_password,mongo_server,mongo_user
-class Mongo():
+class ConnectMongo():
     def __init__(self,host,username,password,database,authMechanism='SCRAM-SHA-1'):
         self.connect_kwargs = {
             'username' : username,
@@ -15,7 +15,7 @@ class Mongo():
         db = connect[self.db_name]
         return db
 
-movie_db = Mongo(host="127.0.0.1:27017",username='poyu2',password='root',database="movie")
+movie_db = ConnectMongo(host="127.0.0.1:27017",username='poyu2',password='root',database="movie")
 movie_mongo_db = movie_db.db_init()
 
 
