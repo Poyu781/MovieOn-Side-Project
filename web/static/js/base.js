a40d7da9f911169400d172c454c8fd18d139914e
Poyu781 <v3708599@gmail.com>
2021-06-28 21:07:51 +0800

    use beautiful file to format code

--- web/static/js/base.js
+++ web/static/js/base.js
@@ -1,24 +1,25 @@
-
 const searchButton = document.querySelector(".button__search")
 
 
 
-searchButton.addEventListener("click",()=>{
+searchButton.addEventListener("click", () => {
     let search_value = document.querySelector("input[type='text']").value
-    if (search_value){
+    if (search_value) {
         window.location.href = `/basicSearch?query=${search_value}`;
     }
 
 })
-document.addEventListener("keypress",(e)=>{
+document.addEventListener("keypress", (e) => {
     if (e.key === 'Enter') {
         let search_value = document.querySelector("input[type='text']").value
-        if (search_value){
+        if (search_value) {
             window.location.href = `/basicSearch?query=${search_value}`;
         }
     }
-}
-)
+})
 document.querySelector(".top__btn").addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  })
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
